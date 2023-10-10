5288e26b28ba36197a8ab8d34cde99cfb2f4fac8
PAguirre <55115279+PauloAguirre@users.noreply.github.com>
2023-10-10 12:18:31 -0700

    Q6 Done

---
+++ packages/express-backend/Express.js
@@ -0,0 +1,10 @@
+const addUser = (user) => {
+    users['users_list'].push(user);
+    return user;
+}
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
